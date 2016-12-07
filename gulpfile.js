c4bb03827b0568d7f0b62b92cd59d2fea53c6142
zhangwei900808 <zw900808@gmail.com>
2016-12-07 14:47:28 +0800

    add file

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require('gulp-rename');
+
+gulp.task('minifyjs', function() {
+	gulp.src('./src/city-picker.js')
+		.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist'))
+})
